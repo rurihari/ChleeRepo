9c3406285126ab8fdf81895bd03fc6e8f61a8798
DATA8320-23 <DATA8320-23@DESKTOP-R25904C>
2020-05-08 11:41:58 +0900

    we

---
+++ JavaPrj/src/Star.java
@@ -0,0 +1,19 @@
+public class Star {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		int j;
+		for(i=0; i<5; i++) {
+			for(j=0; j<i+1; j++)
+				System.out.print("*");
+			System.out.println();
+				
+		}
+		
+		
+		
+		
+	}
+
+}
